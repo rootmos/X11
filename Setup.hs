5f8c0969ed3d80e8e933a9fe4a7d71a63877b9c2
Gustav Behm <gustav.behm@gmail.com>
2016-08-25 17:35:06 +0200

    Add support for building with Stack

--- Setup.hs
+++ Setup.hs
@@ -1,6 +1,15 @@
 module Main (main) where
 
 import Distribution.Simple
+import Distribution.Simple.Setup ( ConfigFlags )
+import Distribution.PackageDescription ( HookedBuildInfo )
+import System.Process ( callCommand )
 
 main :: IO ()
-main = defaultMainWithHooks defaultUserHooks
+main = defaultMainWithHooks autoconfUserHooks { preConf = runAutoconfHook }
+
+runAutoconfHook :: Args -> ConfigFlags -> IO (HookedBuildInfo)
+runAutoconfHook args flags = runAutoconf >> (preConf autoconfUserHooks) args flags
+
+runAutoconf :: IO ()
+runAutoconf = callCommand "autoreconf -i"
